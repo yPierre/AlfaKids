2437f1bc5d3e485ace23c7d36fec3a69bc066dce
yPierre <jeandias1997@gmail.com>
2022-10-03 08:20:52 -0300

    colisao v1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,14 @@ public class PlayerController : MonoBehaviour{
     // Start is called before the first frame update
 
     private CharacterController controller;
+    private Vector3 playerVelocity;
+    private float jumpHeight = 1.0f;
     private Animator anim;
-
     public float speed;
     public float gravity;
     public float rotSpeed;
-
     private float rot;
-    private Vector3 moveDirection;
-
+    //private Vector3 moveDirection;
 
 
     void Start(){
@@ -22,25 +21,36 @@ public class PlayerController : MonoBehaviour{
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
 
+
     }
 
     // Update is called once per frame
     void Update(){
-        
         Move();
-
     }
 
     void Move(){
+
+        Vector3 moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
         if(controller.isGrounded){
             if(Input.GetKey(KeyCode.W)){
-                moveDirection = Vector3.forward * speed;
-                anim.SetInteger("transition", 1);
+                if(Input.GetKey(KeyCode.LeftShift)){
+                    moveDirection = Vector3.forward * speed * 1.5f;
+                    anim.SetInteger("transition", 2); //trocar essa animacao pela de corrida
+                }
+                else{
+                    moveDirection = Vector3.forward * speed;
+                    anim.SetInteger("transition", 1);
+                }
             }
             if(Input.GetKeyUp(KeyCode.W)){
                 moveDirection = Vector3.zero;
                 anim.SetInteger("transition", 0);
             }
+        }
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded){
+            moveDirection.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
         }
 
         rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
@@ -51,4 +61,11 @@ public class PlayerController : MonoBehaviour{
 
         controller.Move(moveDirection * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Fruit"){
+            Destroy(other.gameObject);
+        }
+    }
+
 }
