0b2d543e4a41e1857f9117c06d1c245ccf25e908
yPierre <jeandias1997@gmail.com>
2022-10-04 11:58:00 -0300

    adicionando som das letras

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour{
     public float gravity;
     public float rotSpeed;
     private float rot;
+    public AudioSource audioSource;
+    public AudioClip [] audioCLipArray;
+    public AudioClip B;
     //private Vector3 moveDirection;
 
 
@@ -20,6 +23,9 @@ public class PlayerController : MonoBehaviour{
         
         controller = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
 
 
     }
@@ -66,6 +72,68 @@ public class PlayerController : MonoBehaviour{
         if(other.gameObject.tag == "Fruit"){
             Destroy(other.gameObject);
         }
+        if(other.gameObject.tag.Contains("Letter")){
+            if(other.gameObject.tag.Contains("A"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("B"))
+                audioSource.PlayOneShot(audioCLipArray[1], 0.7f);
+            if(other.gameObject.tag.Contains("C"))
+                audioSource.PlayOneShot(audioCLipArray[2], 0.7f);
+            if(other.gameObject.tag.Contains("D"))
+                audioSource.PlayOneShot(audioCLipArray[3], 0.7f);
+            if(other.gameObject.tag.Contains("E"))
+            /*
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("F"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("G"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("H"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("I"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("J"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("K"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("L"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("M"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("N"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("O"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("P"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("Q"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("R"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("S"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("T"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("U"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("V"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("W"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("X"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("Y"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+            if(other.gameObject.tag.Contains("Z"))
+                audioSource.PlayOneShot(audioCLipArray[0], 0.7f);
+                */
+            Destroy(other.gameObject);
+        }
+        if(other.gameObject.tag == "World"){
+            Destroy(other.gameObject);
+        }
     }
+
+    
 
 }
